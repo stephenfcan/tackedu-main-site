d2c4cbe912f6b80a1f2eba63d25016795cb82ec2
Stephen An <stephenan@Stephens-MacBook-Air.local>
2022-05-29 15:34:51 -0700

    about page fix 1

--- pages/contact.js
+++ pages/contact.js
@@ -9,7 +9,7 @@ function contact() {
         <main>
             <div className="mt-[100px] text-center space-y-9">
                 <h1 className="font-serif leading-tight font-medium text-3xl lg:text-7xl max-w-5xl mx-auto">Contact Us</h1>
-                <p className="px-4 font-medium text-sm lg:text-md text-center mx-auto max-w-3xl">Whatever your comments are, we want to hear from you. Just fill out the feedback form below and we'll respond as soon as possible.</p>
+                <p className="px-4 font-medium text-sm lg:text-md text-center mx-auto max-w-3xl">Whatever your comments are, we want to hear from you. Just fill out the feedback form below and we&apos'll respond as soon as possible.</p>
             </div>
 
             <form action="">
